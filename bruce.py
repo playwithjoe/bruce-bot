4325b2effee230a1b9d996879a20e161d6c43afa
Joseph O'Gorman <joe@megan-menziess-macbook-pro-4.local>
2022-11-17 23:03:39 -0500

    test bot complete

--- bruce.py
+++ bruce.py
@@ -20,4 +20,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('your token here')
+client.run(DISCORD_TOKEN)
